87a5eceda2a4e18771f574afacab0a17a3b62b96
anandak-ops <anandhakrishnan.p3644@gmail.com>
2024-11-04 00:58:44 +0530

    feat: new api route change to version management

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('v1');
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
